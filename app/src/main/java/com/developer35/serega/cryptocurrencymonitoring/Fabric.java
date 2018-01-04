d5210734ce03a8dac597b238bdaf27dc1d798c86
Serega <serv3546@gmail.com>
2018-01-04 12:48:42 -0500

    added retrofit dependencies; created singleton class to init retrofit instance

---
+++ app/src/main/java/com/developer35/serega/cryptocurrencymonitoring/Fabric.java
@@ -0,0 +1,26 @@
+package com.developer35.serega.cryptocurrencymonitoring;
+
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Fabric {
+
+    private static final String BASE_URL = "https://api.coinmarketcap.com/";
+
+    private static Retrofit getRetrofit() {
+
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(logging);
+
+        return new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient.build())
+                .build();
+    }
+}
